fcdde18b8c3392c3ab0b72b98fcd6958c4e1d301
MuFe <MuFe@MuFedeMac-mini.local>
2018-06-07 19:12:33 +0800

    Jcenter提交

--- imageLayout/src/main/java/com/benyi/sdk/imageLayout/listener/ImageLayoutActionListener.java
+++ imageLayout/src/main/java/com/benyi/sdk/imageLayout/listener/ImageLayoutActionListener.java
@@ -16,7 +16,7 @@ public interface ImageLayoutActionListener<T>{
 
     /**
      * 动画状态改变
-     * @param isStart
+     * @param isStart 开始改变
      */
     void animationStateChange(boolean isStart);
 
@@ -47,53 +47,53 @@ public interface ImageLayoutActionListener<T>{
 
     /**
      * 向下滑动响应
-     * @param name
+     * @param name 文件夹名称
      */
     void bottomAction(String name);
 
     /**
      * CardView从外部加载资源
-     * @param data
-     * @param view
+     * @param data  资源文件
+     * @param view  加载的CardView
      */
     void loadResourceToCardView(T data,View view);
 
     /**
      * 初始化全屏View资源
-     * @param data
-     * @param view
+     * @param data 源文件
+     * @param view 全屏的View
      */
     void initfullScreenPhoto(T data,View view);
 
     /**
      * 向CardView中添加View
-     * @param cardView
-     * @param data
+     * @param cardView CardView
+     * @param data 资源文件
      */
     void addViewToCardView(ViewGroup cardView,T data);
 
     /**
      * 获取正在展示的Index
-     * @return
+     * @return 返回index
      */
     int getShowIndex();
 
     /**
      * 获取正在展示的资源
-     * @param position
-     * @return
+     * @param position index
+     * @return 返回资源文件
      */
     T getShowResourceData(int position);
 
     /**
      * 获取向上滑动CardView消失区域
-     * @return
+     * @return 返回区域
      */
     Rect getTopHideRect();
 
     /**
      * 获取向下滑动CardView消失区域
-     * @return
+     * @return 返回区域
      */
     Rect getBottomHideRect();
 }
